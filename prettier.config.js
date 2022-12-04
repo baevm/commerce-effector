81598571f03539825db19afe76550d652245063c
dezzerlol <misha.6325@gmail.com>
2022-12-04 18:18:33 +0500

    initial

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  tabWidth: 2,
+  useTabs: false,
+  jsxBracketSameLine: true,
+  bracketSameLine: true,
+  semi: false,
+  printWidth: 120,
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.js',
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
